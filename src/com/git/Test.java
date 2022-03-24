48c011b2f6d5881cf1699e681a0755e909db6c04
ASHUTOSH <ASHUTOSH@Ashutosh-Lonikar>
2022-03-24 10:40:06 +0530

    This is my first commit....

---
+++ src/com/git/Test.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class Test {
+	
+public static void main(String[] args) {
+	String hungary="yes";
+	if(hungary=="yes") {
+		System.out.println("Eat something......");
+	}
+	else {
+		System.out.println("Do some work.......");
+	}
+}
+}
